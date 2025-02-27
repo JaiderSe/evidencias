de89cd8375f1c3924d10f7da19a5d8edd0b0b015
Sebastián Quintero <154089423+JaiderSe@users.noreply.github.com>
2025-02-26 22:22:26 -0500

    evidencias de la semana 1 y 2

--- src/semana3/uno/Ejercicio1.java
+++ src/semana3/uno/Ejercicio1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication43;
+package semana3;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
--- src/semana3/uno/Ejercicio3.java
+++ src/semana3/uno/Ejercicio3.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication43;
+package semana3;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -17,7 +19,10 @@ public class Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        JOptionPane.showMessageDialog(null, "holaaa como estas?");
     }
+    
     
 }
