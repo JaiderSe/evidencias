170d81c64cdcc33980097483dddcbad99eec2c43
DuvierOlmos <olmos.4561@gmail.com>
2025-03-21 09:18:34 -0500

    trabajo hoy

--- 38362/src/semana6/Ejercicio5/Ejercicio1.java
+++ 38362/src/semana6/Ejercicio5/Ejercicio1.java
@@ -1,5 +1,7 @@
 package semana6.Ejercicio5;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio1 {
 
     Persona obj;
@@ -8,8 +10,10 @@ public class Ejercicio1 {
 
         Persona      obj1=  new       Persona();
     //  tipo de dato objeto operador  metodo
-
- 
+        
+    obj1.setNombre("hola");
+    JOptionPane.showMessageDialog(null, "estado del objeto  "+obj1.getNombre()+obj1.toString());    
+    
         
     }
 
