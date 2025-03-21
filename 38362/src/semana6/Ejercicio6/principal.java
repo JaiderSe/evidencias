b77f450da3dcf5585d8fc412145ee1844ad5c617
DuvierOlmos <olmos.4561@gmail.com>
2025-03-21 09:57:52 -0500

    trabajo

--- 38362/src/semana6/Ejercicio6/principal.java
+++ 38362/src/semana6/Ejercicio6/principal.java
@@ -1,11 +1,14 @@
 package semana6.Ejercicio6;
 
+
+
 public class Principal {
     public static void main(String[] args) {
          
             // Crear instancia con constructor por defecto
             Circulo circulo1 = new Circulo();
-
+            
+            circulo1.setRadio(4);
             System.out.println(circulo1);
             System.out.println("Área: " + circulo1.calcularArea());
             System.out.println("Perímetro: " + circulo1.calcularPerimetro());
