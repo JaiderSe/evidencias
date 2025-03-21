c7a0f8187eb59dfe668b723e66c36eabd65e79f4
DuvierOlmos <olmos.4561@gmail.com>
2025-03-21 08:57:24 -0500

    Agregar Ejercicio 4 con imagen y estructura inicial

---
+++ 38362/src/semana6/Ejercicio5/Ejercicio1.java
@@ -0,0 +1,16 @@
+package semana6.Ejercicio5;
+
+public class Ejercicio1 {
+
+    Persona obj;
+
+    public static void main(String[] args) {
+
+        Persona      obj1=  new       Persona();
+    //  tipo de dato objeto operador  metodo
+
+ 
+        
+    }
+
+}
